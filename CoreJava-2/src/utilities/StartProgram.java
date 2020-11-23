9c39f4fa025538f6c069d2ce80ae590cd3192b63
= <=>
2020-11-23 16:01:29 +0545

    Utility Start program

---
+++ CoreJava-2/src/utilities/StartProgram.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import java.io.IOException;
+
+public class StartProgram {
+
+	public static void main(String[] args) {
+		Runtime rs = Runtime.getRuntime();
+		try {
+			rs.exec("mspaint");
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
