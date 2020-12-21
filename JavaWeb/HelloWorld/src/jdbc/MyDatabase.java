8689d1aae13e328992a3fd2510ef56d573ab1b12
= <=>
2020-12-21 15:57:22 +0545

    Dec 21, 2020 CRUD from Servlet

--- JavaWeb/HelloWorld/src/jdbc/MyDatabase.java
+++ JavaWeb/HelloWorld/src/jdbc/MyDatabase.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyDatabase {
-	
+
 	//Insert Record
 	public boolean insert(Contact contact) {		
 		boolean result=false;
@@ -49,7 +49,7 @@ public class MyDatabase {
 		}
 		return result;
 	}
-	
+
 	//All Records
 	public List get_all() {		
 		List result=new ArrayList();
@@ -74,23 +74,23 @@ public class MyDatabase {
 			Statement stat = conn.createStatement();
 			PreparedStatement pstat = conn.prepareStatement(str_sql);
 			ResultSet rs = pstat.executeQuery(); //executeUpdate-> Insert, Update, Delete Record
-			
+
 			while (rs.next()) {
 				int sn;
 				String name;
 				String address;
 				String email;
 				String phone;
-				
+
 				sn = rs.getInt("sn");
 				name = rs.getString("name");
 				address = rs.getString("address");
 				email = rs.getString("email");
 				phone = rs.getString("phone");
-				
+
 				Contact tmp = new Contact(sn, name, address, email, phone);
 				result.add(tmp);
-				
+
 			}			
 			stat.close();
 			conn.close();
@@ -101,6 +101,133 @@ public class MyDatabase {
 		}
 		return result;
 	}
+
+	public Contact search_contact(int id) {		
+		Contact tmp_contact = new Contact();
+		
+		//Step-1
+		String driver="com.mysql.cj.jdbc.Driver";
+		String host="127.0.0.1";
+		int port = 3306;
+		String db_name="java2";
+		String db_user="admin";
+		String user_pass="admin@123";
+
+		//SQL Statement
+		String str_sql = "SELECT * FROM contacts WHERE sn = ?";
+
+		try {
+			//Step-2
+			//Load Driver
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ db_name +"?", db_user, user_pass);
+
+			//Insert Record
+			Statement stat = conn.createStatement();
+			PreparedStatement pstat = conn.prepareStatement(str_sql);
+			pstat.setInt(1, id);
+			ResultSet rs = pstat.executeQuery(); //executeUpdate-> Insert, Update, Delete Record
+			
+			while (rs.next()) {
+				int sn;
+				String name;
+				String address;
+				String email;
+				String phone;
+
+				sn = rs.getInt("sn");
+				name = rs.getString("name");
+				address = rs.getString("address");
+				email = rs.getString("email");
+				phone = rs.getString("phone");
+
+				tmp_contact = new Contact(sn, name, address, email, phone);
+			}			
+			stat.close();
+			conn.close();
+		}
+		catch(Exception ex) {			
+			System.out.println("Error : "+ex.getMessage());			
+		}
+		return tmp_contact;
+	}
+
 	//Update Record
-	//Delete Record	
+	public boolean update(Contact contact) {		
+		boolean result=false;
+		//Step-1
+		String driver="com.mysql.cj.jdbc.Driver";
+		String host="127.0.0.1";
+		int port = 3306;
+		String db_name="java2";
+		String db_user="admin";
+		String user_pass="admin@123";
+
+		//SQL Statement
+		String str_sql = "UPDATE contacts SET name=?, address=?, email=?, phone=? WHERE sn=?";
+
+		try {
+			//Step-2
+			//Load Driver
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ db_name +"?", db_user, user_pass);
+
+			//Insert Record
+			Statement stat = conn.createStatement();
+			PreparedStatement pstat = conn.prepareStatement(str_sql);
+			pstat.setString(1, contact.getName());
+			pstat.setString(2, contact.getAddress());
+			pstat.setString(3, contact.getEmail());
+			pstat.setString(4, contact.getPhone());
+			pstat.setInt(5, contact.getSn());
+
+			pstat.executeUpdate(); //executeUpdate -> Insert, Update, Delete Record
+			stat.close();
+			conn.close();
+			result=true;
+		}
+		catch(Exception ex) {
+			result=false;
+			System.out.println("Error : "+ex.getMessage());			
+		}
+		return result;
+	}
+	
+	//Delete Record
+	public boolean delete(int id) {		
+		boolean result=false;
+		//Step-1
+		String driver="com.mysql.cj.jdbc.Driver";
+		String host="127.0.0.1";
+		int port = 3306;
+		String db_name="java2";
+		String db_user="admin";
+		String user_pass="admin@123";
+
+		//SQL Statement
+		String str_sql = "DELETE FROM contacts WHERE sn=?";
+
+		try {
+			//Step-2
+			//Load Driver
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ db_name +"?", db_user, user_pass);
+
+			//Insert Record
+			Statement stat = conn.createStatement();
+			PreparedStatement pstat = conn.prepareStatement(str_sql);
+			pstat.setInt(1, id);
+
+			pstat.executeUpdate(); //executeUpdate -> Insert, Update, Delete Record
+			stat.close();
+			conn.close();
+			result=true;
+		}
+		catch(Exception ex) {
+			result=false;
+			System.out.println("Error : "+ex.getMessage());			
+		}
+		return result;
+	}
+
 }
--- JavaWeb/HelloWorld/src/unit1/Six.java
+++ JavaWeb/HelloWorld/src/unit1/Six.java
@@ -23,12 +23,12 @@ public class Six extends GenericServlet {
 		
 		out.println("<a href='form3.jsp'>ADD NEW</a>");				
 		out.println("<table border=1>");
-		out.println("<tr><td>SN</td><td>NAME</td><td>ADDRESS</td><td>EMAIL</td><td>PHONE</td></tr>");		
+		out.println("<tr><td>SN</td><td>NAME</td><td>ADDRESS</td><td>EMAIL</td><td>PHONE</td><td>CONTROL</td></tr>");		
 		
 		for(int i =0; i<contacts.size(); i++) {
 			Contact tmp_contact = (Contact) contacts.get(i); 
 			System.out.println(tmp_contact.getSn());
-			out.println("<tr><td>"+ tmp_contact.getSn() +"</td><td>"+ tmp_contact.getName() +"</td><td>"+ tmp_contact.getAddress() +"</td><td>"+ tmp_contact.getEmail() +"</td><td>"+ tmp_contact.getPhone() +"</td></tr>");
+			out.println("<tr><td>"+ tmp_contact.getSn() +"</td><td>"+ tmp_contact.getName() +"</td><td>"+ tmp_contact.getAddress() +"</td><td>"+ tmp_contact.getEmail() +"</td><td>"+ tmp_contact.getPhone() +"</td><td><a href='seven?id="+ tmp_contact.getSn() +"'>EDIT</a> | <a href='nine?id="+ tmp_contact.getSn() +"'>DELETE</a> </td></tr>");
 		}		
 		out.println("</table");
 	}
